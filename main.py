a61de4e72d3d88f39c38c833f7747f4ea514bbf2
Ike Maldonado <maldonadoike@gmail.com>
2024-06-10 06:13:58 -0700

    base

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+
+import models
+from models import User, Bodypart, UserBodypart
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+# pydantic
+class UserCreate(BaseModel):
+    username: str = Field(min_length=1, max_length=20)
+
+
+@app.post("/user/")
+def create_user(user_data: UserCreate, db: db_dependency):
+    user = User(**user_data.dict())
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
+@app.get("/users")
+def read_users(db: db_dependency):
+    user_model = db.query(User).all()
+    return user_model
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DATETIME, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, DATETIME, Boolean, PrimaryKeyConstraint
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -8,10 +8,9 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
-    email = Column(String, unique=True)
     hashed_password = Column(String)
     role = Column(String)
-    created_at = Column(DATETIME, nullable=False)
+    created_at = Column(DATETIME, nullable=True)
 
 
 class Bodypart(Base):
@@ -19,3 +18,22 @@ class Bodypart(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
+
+
+class UserBodypart(Base):
+    __tablename__ = 'user_bodypart'
+
+    user_id = Column(Integer, ForeignKey("user.id"), primary_key=True)
+    bodypart_id = Column(Integer, ForeignKey("bodypart.id"), primary_key=True)
+    last_exercise = Column(DATETIME, nullable=True)
+    primary_or_secondary = Column(String, nullable=True)
+
+    user = relationship("User", back_populates="user_bodypart")
+    bodypart = relationship("Bodypart", back_populates="user_bodypart")
+
+    __table_args__ = (PrimaryKeyConstraint("user_id", "bodypart_id"),)
+
+
+# Backref relationships
+User.user_bodypart = relationship("UserBodypart", back_populates="user")
+Bodypart.user_bodypart = relationship("UserBodypart", back_populates="bodypart")
